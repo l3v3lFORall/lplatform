131dd98259717d1a496fbcf74e92a87a23f86ae7
l3v3l_l3v3l <linxiaotian1997@gmail.com>
2020-11-06 14:47:44 +0800

    +django demo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Community Online For Team
 ## Env
-+ language: php+mysql+nginx/apache+docker
++ languager:    Django test
---
+++ lplatform/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path,re_path,include
+
+from . import views
+
+urlpatterns = [
+    url('hello/',views.hello),
+    # path('what/',include('otherwebsite.urls')),
+    # import first like line-2
+    # re_path('what/', include('otherwebsite.urls')),
+]
